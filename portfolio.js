49886f3bcb2314e2b597fc4c91f954d862b4f59a
ab-7607 <157165118+ab-7607@users.noreply.github.com>
2024-01-20 00:41:45 +0300

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+
+const menu=document.querySelector('.side-js');
+function openMenu(){
+  menu.style.right="0";
+}
+function closeMenu(){
+  menu.style.right="-150px";
+}
+const tabLinks=document.querySelector('.tab-skill')
+const tabContants=document.querySelector('.skill-me')
+function openTab(tabname){
+  for (tabLinks of tabLinks)   
+  tabLink.classList.remove("skill");
+
+
+  for (tabContant of tabContants)   
+  tabContant.classList.remove("Active-link");
+}
